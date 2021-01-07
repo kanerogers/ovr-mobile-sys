3e6bdc6b45de65b8b04f52538394916b005248b6
Will Brickner <wgbrickner@gmail.com>
2021-01-06 18:51:03 -0600

    Emptied helpers.rs, fixed linking portion of build script

--- build.rs
+++ build.rs
@@ -6,13 +6,12 @@ fn main() {
 
     // Export shared libraries search path.
     if android {
-        let abi = if target.contains("aarch64") {
-            "arm64-v8a"
-        } else {
-            "armeabi-v7a"
+        let abi = {
+            if target.contains("aarch64") { "arm64-v8a" } 
+            else { "armeabi-v7a" }
         };
         
-        println!("cargo:rustc-link-search={}/VrApi/Libs/Android/{}/Release", env!("CARGO_MANIFEST_DIR"), abi);
-        println!("cargo:rustc-link-lib=vrapi");
+        println!("cargo:rustc-link-search={}/VrApi-1.40/Libs/Android/{}/Release", env!("CARGO_MANIFEST_DIR"), abi);
+        println!("cargo:rustc-link-lib=dylib=vrapi");
     }
 }
